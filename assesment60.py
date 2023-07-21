1ac42498d9668a424a2f9365ed5ce6d941b79061
captainversion <136670723+captainversion@users.noreply.github.com>
2023-07-21 19:39:41 +0530

    Add files via upload

---
+++ assesment60.py
@@ -0,0 +1,31 @@
+import json
+
+class Employee:
+    def _init_(self, name, dob, height, city, state):
+        self.name = name
+        self.dob = dob
+        self.height = height
+        self.city = city
+        self.state = state
+
+
+with open("OneDrive/pyhton screen shot/employee.json") as file:
+    data = json.load(file)
+
+employee_list = []
+for employee_data in data["employees"]:
+    name = employee_data["Name"]
+    dob = employee_data["DOB"]
+    height = employee_data["Height"]
+    city = employee_data["City"]
+    state = employee_data["State"]
+    employee = Employee(name, dob, height, city, state)
+    employee_list.append(employee)
+
+for employee in employee_list:
+    print("Name:", employee.name)
+    print("DOB:",employee.dob)
+    print("Height:",employee.height)
+    print("City:",employee.city)
+    print("State:",employee.state)
+    print()
